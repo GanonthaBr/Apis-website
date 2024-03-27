5adbb152daa2fa31b755a79926f5d546ef96e5fd
Bruno <bganontha@gmail.com>
2024-03-27 12:48:54 +0100

    Refactor code to improve performance and readability

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -10,7 +10,7 @@
         <!-- Fin  Bar de navigation  -->
 
         <!-- Debut de Hero -->
-    @include('partials.hero')
+        @include('partials.hero')
         <!-- Fin de Hero -->
 
         <!-- Video Modal Start-->
@@ -50,11 +50,11 @@
         <!-- Video Modal End -->
 
         <!-- Debut de About -->
-     @include('partials.about')
+        @include('partials.about')
         <!-- Fin de About-->
 
         <!-- Debut des Services -->
-       @include('partials.services')
+         @include('partials.services')
         <!-- Fin des Services -->
 
         <!-- Facts Start -->
@@ -269,91 +269,9 @@
         </div>
         <!-- Causes End -->
 
-        <!-- Donate Start -->
-        <div
-            class="donate"
-            data-parallax="scroll"
-            data-image-src="img/donate.jpg"
-        >
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-7">
-                        <div class="donate-content">
-                            <div class="section-header">
-                                <p>Donate Now</p>
-                                <h2>
-                                    Let's donate to needy people for better
-                                    lives
-                                </h2>
-                            </div>
-                            <div class="donate-text">
-                                <p>
-                                    Lorem ipsum dolor sit amet elit. Phasellus
-                                    nec pretium mi. Curabitur facilisis ornare
-                                    velit non. Aliquam metus tortor, auctor id
-                                    gravida, viverra quis sem. Curabitur non
-                                    nisl nec nisi maximus. Aenean convallis
-                                    porttitor. Aliquam interdum at lacus non
-                                    blandit.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-5">
-                        <div class="donate-form">
-                            <form>
-                                <div class="control-group">
-                                    <input
-                                        type="text"
-                                        class="form-control"
-                                        placeholder="Name"
-                                        required="required"
-                                    />
-                                </div>
-                                <div class="control-group">
-                                    <input
-                                        type="email"
-                                        class="form-control"
-                                        placeholder="Email"
-                                        required="required"
-                                    />
-                                </div>
-                                <div
-                                    class="btn-group btn-group-toggle"
-                                    data-toggle="buttons"
-                                >
-                                    <label class="btn btn-custom active">
-                                        <input
-                                            type="radio"
-                                            name="options"
-                                            checked
-                                        />
-                                        $10
-                                    </label>
-                                    <label class="btn btn-custom">
-                                        <input type="radio" name="options" />
-                                        $20
-                                    </label>
-                                    <label class="btn btn-custom">
-                                        <input type="radio" name="options" />
-                                        $30
-                                    </label>
-                                </div>
-                                <div>
-                                    <button
-                                        class="btn btn-custom"
-                                        type="submit"
-                                    >
-                                        Donate Now
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Donate End -->
+        <!-- Debut de Donate -->
+         @include('partials.donate')
+        <!-- Fin de Donate End -->
 
         <!-- Event Start -->
         <div class="event">
