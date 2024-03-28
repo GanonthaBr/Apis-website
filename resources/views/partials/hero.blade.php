3282e3d401cc027a2e8757ee118b19e70f496612
Bruno <bganontha@gmail.com>
2024-03-28 11:47:57 +0100

    Update carousel images and descriptions

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -3,14 +3,13 @@
                 <div class="owl-carousel">
                     <div class="carousel-item">
                         <div class="carousel-img">
-                            <img src="img/carousel-3.png" alt="Image" />
+                            <img src="img/carousel-5.png" alt="Image" />
                         </div>
                         <div class="carousel-text">
-                            <h1>Let's be kind for children</h1>
+                            <h1>Sécurité alimentaire pour tous.</h1>
                             <p>
-                                Lorem ipsum dolor sit amet elit. Phasellus ut
-                                mollis mauris. Vivamus egestas eleifend dui ac
-                                consequat at lectus in malesuada
+                        {{-- Ecrire une breve description sur la securite alimentaire --}}
+                              La sécurité alimentaire est une condition préalable à la santé et au bien-être de tous. APIS œuvre pour un accès physique, social et économique à une nourriture suffisante, saine et nutritive pour les communautés rurales.
                             </p>
                             <div class="carousel-btn">
                                 <a class="btn btn-custom" href="">Donate Now</a>
@@ -29,11 +28,11 @@
                             <img src="img/carousel-2.png" alt="Image" />
                         </div>
                         <div class="carousel-text">
-                            <h1>Get Involved with helping hand</h1>
+                            <h1>Urgence Humanitaire</h1>
                             <p>
-                                Morbi sagittis turpis id suscipit feugiat.
-                                Suspendisse eu augue urna. Morbi sagittis, orci
-                                sodales varius fermentum, tortor
+                                {{-- Ecrire en 2 phrases une description de APIS oeuvrant pour l'urgence humanitaire --}}
+                                APIS intervient dans les situations d'urgence humanitaire pour sauver des vies, soulager la souffrance et préserver la dignité des populations affectées par les conflits armés, les catastrophes naturelles et les épidémies.
+                                
                             </p>
                             <div class="carousel-btn">
                                 <a class="btn btn-custom" href="">Donate Now</a>
@@ -52,11 +51,32 @@
                             <img src="img/carousel-4.png" alt="Image" />
                         </div>
                         <div class="carousel-text">
-                            <h1>Bringing smiles to millions</h1>
+                            <h1>Santé et Nutrition</h1>
                             <p>
-                                Sed ultrices, est eget feugiat accumsan, dui
-                                nibh egestas tortor, ut rhoncus nibh ligula
-                                euismod quam. Proin pellentesque odio
+                                {{-- Ecrire en 2 phrases une description de APIS oeuvrant dans la nutrition et la sante --}}
+                                APIS s'engage à améliorer la santé et la nutrition des populations rurales en leur donnant accès à des services de santé de qualité et à une alimentation saine et nutritive.
+                            </p>
+                            <div class="carousel-btn">
+                                <a class="btn btn-custom" href="">Donate Now</a>
+                                <a
+                                    class="btn btn-custom btn-play"
+                                    data-toggle="modal"
+                                    data-src="https://youtu.be/i78losNIjIY"
+                                    data-target="#videoModal"
+                                    >Watch Video</a
+                                >
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="carousel-img">
+                            <img src="img/carousel-1.png" alt="Image" />
+                        </div>
+                        <div class="carousel-text">
+                            <h1>Éducation et Développement </h1>
+                            <p>
+                                {{-- Ecrire en 2 phrases une description de APIS oeuvrant pour  l'education et le developpement --}}
+                                APIS s'engage à améliorer l'accès à une éducation de qualité pour les enfants et les jeunes des communautés rurales, en particulier les filles, pour leur permettre de réaliser leur plein potentiel et de contribuer au développement de leur communauté.
                             </p>
                             <div class="carousel-btn">
                                 <a class="btn btn-custom" href="">Donate Now</a>
