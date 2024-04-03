b39cae65af5c5fd35f7c3f08b4a4ed4dc91a6fc2
Bruno <bganontha@gmail.com>
2024-04-03 10:39:16 +0100

    Update testimonials page with French translations

--- resources/views/partials/testimonials.blade.php
+++ resources/views/partials/testimonials.blade.php
@@ -1,61 +1,59 @@
      <div class="testimonial">
             <div class="container">
                 <div class="section-header text-center">
-                    <p>Testimonial</p>
+                    <p>Témoignages</p>
                     <h2>
-                        What people are talking about our charity activities
+                        Ce que les gens disent de nos actions humanitaires.
                     </h2>
                 </div>
                 <div class="owl-carousel testimonials-carousel">
                     <div class="testimonial-item">
                         <div class="testimonial-profile">
-                            <img src="img/testimonial-1.jpg" alt="Image" />
+                            <img src="img/testimonial-1.png" alt="Image" />
                             <div class="testimonial-name">
-                                <h3>Person Name</h3>
-                                <p>Profession</p>
+                                <h3>Abdoulaye Djibo</h3>
+                                <p>Paysan</p>
                             </div>
                         </div>
                         <div class="testimonial-text">
                             <p>
-                                Lorem ipsum dolor sit amet elit. Phasel preti mi
-                                facilis ornare velit non vulputa. Aliqu metus
-                                tortor, auctor id gravid vivera quis
+                                
+                                Je suis très reconnaissant envers l'association pour le don de vivres que j'ai reçu. Cela m'a permis de nourrir ma famille pendant plusieurs jours. Merci beaucoup.
+
+                                
                             </p>
                         </div>
                     </div>
                     <div class="testimonial-item">
                         <div class="testimonial-profile">
-                            <img src="img/testimonial-2.jpg" alt="Image" />
+                            <img src="img/testimonial-2.png" alt="Image" />
                             <div class="testimonial-name">
-                                <h3>Person Name</h3>
-                                <p>Profession</p>
+                                <h3>Mariama Kadri</h3>
+                                <p>Paysane</p>
                             </div>
                         </div>
                         <div class="testimonial-text">
                             <p>
-                                Lorem ipsum dolor sit amet elit. Phasel preti mi
-                                facilis ornare velit non vulputa. Aliqu metus
-                                tortor, auctor id gravid vivera quis
+                                Je suis très reconnaissante envers l'association pour le don de vivres que j'ai reçu. Cela m'a permis de nourrir ma famille pendant plusieurs jours. Merci beaucoup.
                             </p>
                         </div>
                     </div>
                     <div class="testimonial-item">
                         <div class="testimonial-profile">
-                            <img src="img/testimonial-3.jpg" alt="Image" />
+                            <img src="img/testimonial-3.png" alt="Image" />
                             <div class="testimonial-name">
-                                <h3>Person Name</h3>
-                                <p>Profession</p>
+                                <h3>Hadiza Liman</h3>
+                                <p>Paysane</p>
                             </div>
                         </div>
                         <div class="testimonial-text">
                             <p>
-                                Lorem ipsum dolor sit amet elit. Phasel preti mi
-                                facilis ornare velit non vulputa. Aliqu metus
-                                tortor, auctor id gravid vivera quis
+                              
+                              Je suis très reconnaissante envers l'association pour le don de vivres que j'ai reçu. Cela m'a permis de nourrir ma famille pendant plusieurs jours. Merci beaucoup.
                             </p>
                         </div>
                     </div>
-                    <div class="testimonial-item">
+                    {{-- <div class="testimonial-item">
                         <div class="testimonial-profile">
                             <img src="img/testimonial-4.jpg" alt="Image" />
                             <div class="testimonial-name">
@@ -70,7 +68,7 @@
                                 tortor, auctor id gravid vivera quis
                             </p>
                         </div>
-                    </div>
+                    </div> --}}
                 </div>
             </div>
         </div>
