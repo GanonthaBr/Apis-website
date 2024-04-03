9911394deb11051c641667206542caaaca927240
Bruno <bganontha@gmail.com>
2024-04-03 16:37:51 +0100

    Refactor navbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 
      <div class="navbar navbar-expand-lg bg-dark navbar-dark">
-            <div class="container-fluid">
+            {{-- <div class="container-fluid">
                 <a href="/" class="navbar-brand">
                     <img src="img/apis.png" alt="">
                 </a>
@@ -59,6 +59,82 @@
                                     >Urgence humanitaire</a
                                 >
                                 <a href="/team" class="dropdown-item"
+                                    >Éducation et développement Social</a
+                                >
+                               
+                            </div>
+                    </div> 
+                        <a href="/events" class="nav-item nav-link"
+                            >Événements</a
+                        >
+                        <a href="/blogs" class="nav-item nav-link">Actualités</a>
+                        <a href="/contacts" class="nav-item nav-link"
+                            >Contact</a
+                        >
+                    </div>
+                </div>
+            </div> --}}
+             <div class="container-fluid">
+                <a href="/" class="navbar-brand">
+                    <img src="img/apis.png" alt="">
+                </a>
+                <button
+                    type="button"
+                    class="navbar-toggler"
+                    data-toggle="collapse"
+                    data-target="#navbarCollapse"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div
+                    class="collapse navbar-collapse justify-content-between"
+                    id="navbarCollapse"
+                >
+                    <div class="navbar-nav ml-auto">
+                        <a href="/" class="nav-item nav-link active"
+                            >Accueil</a
+                        >
+
+                    <div class="nav-item dropdown">
+                            <a
+                                href="#"
+                                class="nav-link dropdown-toggle"
+                                data-toggle="dropdown"
+                                >Qui sommes-nous?</a
+                            >
+                            <div class="dropdown-menu">
+                                <a href="/about-us" class="dropdown-item"
+                                    >Mot du Secrétaire</a
+                                >
+                                <a href="/about" class="dropdown-item"
+                                    >Notre histoire</a
+                                >
+                                <a href="/team" class="dropdown-item"
+                                    >Notre Équipe</a
+                                >
+                               
+                            </div>
+                        </div> 
+                        
+                        <div class="nav-item dropdown">
+                            <a
+                                href="#"
+                                class="nav-link dropdown-toggle"
+                                data-toggle="dropdown"
+                                >Domaines d'intervention</a
+                            >
+                            <div class="dropdown-menu">
+                                <a href="/securitealimentaire" class="dropdown-item"
+                                    >Sécurité alimentaire</a
+                                >
+                                <a href="/santenutrition" class="dropdown-item"
+                                    >Santé et Nutrition</a
+                                >
+                                <a href="/urgencehumanitaire" class="dropdown-item"
+                                    >Urgence humanitaire</a
+                                >
+                                <a href="/educationdeveloppement" class="dropdown-item"
                                     >Éducation et développement Social</a
                                 >
                                
