3f13359cf8a687eca3a32e356558a98711894b1d
Bruno <bganontha@gmail.com>
2024-04-03 11:32:47 +0100

    Update event titles and descriptions

--- resources/views/partials/events/index.blade.php
+++ resources/views/partials/events/index.blade.php
@@ -20,40 +20,37 @@
         </div>
         <!-- Page Header End -->  
 
-  {{-- Events Starts --}}
+   {{-- Events Starts --}}
   <div class="event">
             <div class="container">
                 <div class="section-header text-center">
-                    <p>Upcoming Events</p>
-                    <h2>Be ready for our upcoming charity events</h2>
+                    <p>Nos événements récents</p>
+                    <h2>Nos plus récents et futurs événements.</h2>
                 </div>
                 <div class="row">
                     <div class="col-lg-6">
                         <div class="event-item">
-                            <img src="img/event-1.jpg" alt="Image" />
+                            <img src="img/event-1-0.png" alt="Image" />
                             <div class="event-content">
                                 <div class="event-meta">
                                     <p>
                                         <i class="fa fa-calendar-alt"></i
-                                        >01-Jan-45
+                                        >01-Mars-2024
                                     </p>
                                     <p>
                                         <i class="far fa-clock"></i>8:00 - 10:00
                                     </p>
                                     <p>
-                                        <i class="fa fa-map-marker-alt"></i>New
-                                        York
+                                    Gotheye-larbabirno
                                     </p>
                                 </div>
                                 <div class="event-text">
-                                    <h3>Lorem ipsum dolor sit</h3>
+                                    <h3>Distribution</h3>
                                     <p>
-                                        Lorem ipsum dolor sit amet elit. Neca
-                                        pretim miura bitur facili ornare velit
-                                        non vulpte liqum metus tortor
+                                      Événement de distribution des kits non-alimentaires à la population du village de Larbabirno.
                                     </p>
                                     <a class="btn btn-custom" href=""
-                                        >Join Now</a
+                                        >Voir plus</a
                                     >
                                 </div>
                             </div>
@@ -61,30 +58,27 @@
                     </div>
                     <div class="col-lg-6">
                         <div class="event-item">
-                            <img src="img/event-2.jpg" alt="Image" />
+                            <img src="img/event-1-1.png" alt="Image" />
                             <div class="event-content">
                                 <div class="event-meta">
                                     <p>
                                         <i class="fa fa-calendar-alt"></i
-                                        >01-Jan-45
+                                        >01-Mars-2024
                                     </p>
                                     <p>
                                         <i class="far fa-clock"></i>8:00 - 10:00
                                     </p>
                                     <p>
-                                        <i class="fa fa-map-marker-alt"></i>New
-                                        York
+                                        <i class="fa fa-map-marker-alt"></i>Gotheye-larbabirno
                                     </p>
                                 </div>
                                 <div class="event-text">
-                                    <h3>Lorem ipsum dolor sit</h3>
+                                    <h3>Distribution</h3>
                                     <p>
-                                        Lorem ipsum dolor sit amet elit. Neca
-                                        pretim miura bitur facili ornare velit
-                                        non vulpte liqum metus tortor
+                                      Événement de distribution des kits non-alimentaires à la population du village de Larbabirno.
                                     </p>
                                     <a class="btn btn-custom" href=""
-                                        >Join Now</a
+                                        >Voir plus</a
                                     >
                                 </div>
                             </div>
