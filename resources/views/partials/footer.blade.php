39171be4c4696e7f09520efb39a0eb24af672d04
Bruno <bganontha@gmail.com>
2024-04-03 10:50:35 +0100

    Created footer file and changes the description

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,81 @@
+  <div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-contact">
+                            <h2>Siège Principal</h2>
+                            <p>
+                                <i class="fa fa-map-marker-alt"></i>Avenue Korey bongou</p>
+                            <p><i class="fa fa-phone-alt"></i>(+227) 20724108 - 96410772</p>
+                            <p>
+                                <i class="fa fa-envelope"></i>contact@apis-sahel.org</p>
+                            <div class="footer-social">
+                                <a class="btn btn-custom" href=""
+                                    ><i class="fab fa-twitter"></i
+                                ></a>
+                                <a class="btn btn-custom" href=""
+                                    ><i class="fab fa-facebook-f"></i
+                                ></a>
+                                <a class="btn btn-custom" href=""
+                                    ><i class="fab fa-youtube"></i
+                                ></a>
+                                <a class="btn btn-custom" href=""
+                                    ><i class="fab fa-instagram"></i
+                                ></a>
+                                <a class="btn btn-custom" href=""
+                                    ><i class="fab fa-linkedin-in"></i
+                                ></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-link">
+                            <h2>Liens Utiles</h2>
+                            <a href="">Mot du Secrétaire Général</a>
+                            <a href="">À propos</a>
+                            <a href="">Récents Événements</a>
+                            <a href="">Contacts</a>
+                            <a href="">Dernières actualités</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-link">
+                            <h2>Useful Links</h2>
+                            <a href="">Terms of use</a>
+                            <a href="">Privacy policy</a>
+                            <a href="">Cookies</a>
+                            <a href="">Help</a>
+                            <a href="">FQAs</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="footer-newsletter">
+                            <h2>Newsletter</h2>
+                            <form>
+                                <input
+                                    class="form-control"
+                                    placeholder="Email goes here"
+                                />
+                                <button class="btn btn-custom">Submit</button>
+                                <label>Don't worry, we don't spam!</label>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container copyright">
+                <div class="row">
+                    <div class="col-md-6">
+                        <p>
+                           {{Date('Y')}} &copy; <a href="https://apis-sahel.org">Apis Sahel  </a>, Tout Droit réservé.
+                        </p>
+                    </div>
+                    <div class="col-md-6">
+                        <p>
+                            Conçu  par
+                            <a href="https://apis-sahel.org">Apis Sahel</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
