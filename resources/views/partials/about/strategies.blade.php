f4ad8eff2a7e574392bda0e7c7e7d306255a962c
Bruno <bganontha@gmail.com>
2024-04-05 09:54:42 +0100

    Add new images and update content in about/strategies.blade.php

--- resources/views/partials/about/strategies.blade.php
+++ resources/views/partials/about/strategies.blade.php
@@ -1,33 +1,56 @@
 <div class="container strategies">
         <ul class="nav nav-pills nav-justified">
             <li class="nav-item">
-                <a class="nav-link active" data-toggle="pill" href="#tab1">Tab 1</a>
+                <a class="nav-link active" data-toggle="pill" href="#tab1">Stratégies</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" data-toggle="pill" href="#tab2">Tab 2</a>
+                <a class="nav-link" data-toggle="pill" href="#tab2">Approche</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" data-toggle="pill" href="#tab3">Tab 3</a>
             </li>
         </ul>
-        <div class="tab-content">
+        <div class="tab-content pt-6">
             <div id="tab1" class="container tab-pane active row">
         <div class="row">
             <div class="col-6">
                 <img src="img/carousel-4.png" alt="Image 1">
             </div>
             <div class="col">
-                <p>1APIS développe un partenariat avec les institutions nationales, internationales, publiques et privées et toutes les structures œuvrant dans les domaines de la sécurité alimentaire, santé et nutrition et le développement intégré ;</p>
+                <p>APIS développe un partenariat avec les institutions nationales, internationales, publiques et privées et toutes les structures œuvrant dans les domaines de la sécurité alimentaire, santé et nutrition et le développement intégré ;</p>
+
+                <p> 
+                    APIS bâtit des partenariats avec les institutions gouvernementales et non gouvernementales ainsi que des institutions onusiennes très engagées dans la sécurité alimentaire afin de mettre en œuvre des projets et pro- grammes qui marquent d’impact pour le bien être du monde rural ;
+                </p>
+                <p>
+                    L’ONG a aussi ouvert des bureaux de terrain (Tillabéry, Maradi et Tahoua) ce qui lui permet de mieux servir les communautés bénéficiaires dans ces régions.
+                </p>
             </div>
         </div>
             </div>
             <div id="tab2" class="container tab-pane row"> 
          <div class="row">
               <div class="col-6">
-                     <img src="img/carousel-4.png" alt="Image 2">
+                <div class="row">
+                    <img src="img/approche-1.png" alt="Image 2" class="py-3">
+                </div>
+                <div class="row">
+                    <img src="img/approche-2.png" alt="Image 2" class="py-3">
+                </div>
               </div>
               <div class="col">
-                     <p>2APIS développe un partenariat avec les institutions nationales, internationales, publiques et privées et toutes les structures œuvrant dans les domaines de la sécurité alimentaire, santé et nutrition et le développement intégré ;</p>
+                <h3>Principe de base</h3>
+                     <p>Dans la mise en œuvre des activités, l’ONG met au centre, le bénéficiaire du projet et programme tout en mettant l’accent sur les dispositifs de participation et de suivi des activités afin de renforcer les capacités des bénéficiaires pour leur participation dans toutes les phases du projet ou du programme.</p>
+                <h3>Dans la perspective d’impulser le développement intégré</h3>
+                <p>
+                    APIS privilégie l’approche holistique (pluridisciplinarité) dans son intervention. APIS utilise des outils appropriés aux contextes et situations et au renforcement des capacités qui varient d’un domaine à un autre tels que :
+                    <ul>
+                        <li>La Gestion Axée sur les Résultats de Développement (GARD);</li>
+                        <li>L’Analyse des facteurs sociaux, technologiques, économiques et politiques (STEP);</li>
+                        <li>La Méthodes Accélérée de Recherche Participative (MARP/PRA/RRA);</li>
+                        <li>Les Méthodes d’évaluation et de recherche de solutions en Sécurité Alimentaire dans les pays en voie de développement à travers l’analyse (AFOM) et Connaissances, Attitudes et Pratiques (CAP).</li>
+                    </ul>
+                </p>
                </div>
          </div>
             </div>
