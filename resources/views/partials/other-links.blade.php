bc079e20bc528497da3acc4a55f3a0a8f431aef4
Bruno <bganontha@gmail.com>
2024-04-17 17:05:27 +0100

    Update links and routes for education and development section

--- resources/views/partials/other-links.blade.php
+++ resources/views/partials/other-links.blade.php
@@ -1,6 +1,6 @@
 <div class="single-tags">
-    <a href="">Santé & Nutrition</a>
-    <a href="">Urgence humanitaire</a>
-    <a href="">Sécurité Alimentaire</a>
-    <a href="">Education et Développement social</a>
+    <a href="{{route('domaines.santenutrition')}}">Santé & Nutrition</a>
+    <a href="{{route('domaines.urgencehumanitaire')}}">Urgence humanitaire</a>
+    <a href="{{route('domaines.securitealimentaire')}}">Sécurité Alimentaire</a>
+    <a href="{{route('domaines.educationdeveloppement')}}">Education et Développement social</a>
 </div>
