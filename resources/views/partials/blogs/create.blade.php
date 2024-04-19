f08a33e62ee8caca3c50111d36a3f06ba62b93ce
Bruno <bganontha@gmail.com>
2024-04-19 11:56:55 +0100

    Add create blog form view

---
+++ resources/views/partials/blogs/create.blade.php
@@ -0,0 +1,16 @@
+@include('layouts.layout')
+
+@section('content')
+<div class="container">
+    <form action="{{ route('blogs.store') }}" method="POST" enctype="multipart/form-data">
+    @csrf
+    <label for="title">Titre:</label>
+    <input type="text" id="title" name="title" placeholder="Entrer le titre de la publication" required>
+    <label for="content">Contenu:</label>
+    <textarea id="content" name="content" placeholder="Entrer le contenu ici" required></textarea>
+    <label for="image">Image:</label>
+    <input type="file" id="image" name="image">
+    <button type="submit" class="btn btn-primary" >Publier</button>
+</form>
+</div>
+@endsection
