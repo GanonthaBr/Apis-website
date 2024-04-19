cca23732ace6dace5586ce525f269c6f93bb0849
Bruno <bganontha@gmail.com>
2024-04-19 11:29:43 +0100

    Add route and controller method for displaying individual blog details

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
@@ -10,5 +11,11 @@ class BlogController extends Controller
     public function index()
     {
         return view('partials.blogs.index');
+    }
+    //display a single post
+    public function show($id)
+    {
+        $post = Blog::findOrFail($id);
+        return view('partials.blogs.blog-details', ['post' => $post]);
     }
 }
