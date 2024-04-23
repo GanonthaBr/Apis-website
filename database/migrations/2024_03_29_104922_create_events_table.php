38f20d5b7912bda97ac62132c41a72c9e4d41733
Bruno <bganontha@gmail.com>
2024-04-23 16:06:19 +0100

    Update Events model to use 'events' table

--- database/migrations/2024_03_29_104922_create_events_table.php
+++ database/migrations/2024_03_29_104922_create_events_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('location');
             $table->dateTime('start_time');
             $table->dateTime('end_time');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
