844ca95efaf46ba942e4a4ef5ca0b50277c81f27
Bruno <bganontha@gmail.com>
2024-04-23 16:05:03 +0100

    Add migration to create events table

--- database/migrations/2024_03_29_104922_create_events_table.php
+++ database/migrations/2024_03_29_104922_create_events_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->date('date');
+            $table->string('title');
+            $table->text('description');
+            $table->string('location');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
             $table->timestamps();
         });
     }
