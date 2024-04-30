e85db4e9aac824be69bb2fc21c2198f2e9d0d143
Bruno <bganontha@gmail.com>
2024-04-30 12:02:04 +0100

    Add create testimonial functionality to TestimonialsController

--- app/Http/Controllers/TestimonialsController.php
+++ app/Http/Controllers/TestimonialsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Testimonials;
 use Illuminate\Http\Request;
+use Dotenv\Exception\ValidationException;
 
 class TestimonialsController extends Controller
 {
@@ -12,5 +13,32 @@ class TestimonialsController extends Controller
     {
         $testimonials = Testimonials::latest()->take(3)->get();
         return view('testimonials', compact('testimonials'));
+    }
+    //create new testimonial
+    public function create()
+    {
+        return view('partial.testimonials.create');
+    }
+
+    // store a new testimonial
+    // the authorImage should be optional and have a default value
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                'authorName' => 'required',
+                'authorPosition' => 'required',
+                'content' => 'required'
+            ]);
+            $testimonial = new Testimonials();
+            $testimonial->authorName = $request->authorName;
+            $testimonial->authorImage = $request->authorImage ?? 'default.jpg';
+            $testimonial->authorPosition = $request->authorPosition;
+            $testimonial->content = $request->content;
+            $testimonial->save();
+            return redirect('home');
+        } catch (ValidationException $e) {
+            return response()->json(['message' => $e->getMessage()]);
+        }
     }
 }
