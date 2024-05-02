375ee6b1f54a0c07837d28072df531cec6621f48
Bruno <bganontha@gmail.com>
2024-05-02 09:11:45 +0100

    Add testimonials functionality to home page and testimonials directory

--- resources/views/partials/contacts/index.blade.php
+++ resources/views/partials/contacts/index.blade.php
@@ -1,138 +1,99 @@
-@extends('layouts.layout') 
-  @section('content')
-            @include('includes.topbar')
-            @include('includes.navbar')
+@extends('layouts.layout')
+@section('content')
+@include('includes.topbar')
+@include('includes.navbar')
 
-      <!-- Page Header Start -->
-        <div class="page-header">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <h2>Nous Contacter</h2>
-                    </div>
-                    <div class="col-12">
-                        <a href="/">Accueil</a>
-                        <a href="/contacts">Contacts</a>
-                    </div>
-                </div>
+<!-- Page Header Start -->
+<div class="page-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2>Nous Contacter</h2>
+            </div>
+            <div class="col-12">
+                <a href="/">Accueil</a>
+                <a href="/contacts">Contacts</a>
             </div>
         </div>
-        <!-- Page Header End -->  
+    </div>
+</div>
+<!-- Page Header End -->
 
-        <!-- Contacts Start -->
-           <div class="contact partial-content">
-            <div class="container">
-                <div class="section-header text-center">
-                    <p>Nous Contacter</p>
-                    <h2>Envoyez nous un message</h2>
+<!-- Contacts Start -->
+<div class="contact partial-content">
+    <div class="container">
+        <div class="section-header text-center">
+            <p>Nous Contacter</p>
+            <h2>Restez en contact avec nous</h2>
+        </div>
+        <div class="contact-img">
+            <img src="{{asset('img/cause-3.png')}}" alt="Image" />
+        </div>
+    <div class="row">
+            <div class="col-6">
+              
+                <div class="contact-form">
+                <h4 class="text-center">Envoyez nous un message</h4>
+                <div id="success">
+
+                @if(session('success'))
+
+                <div class="alert alert-success"> {{session('success')}}</div>
+                @endif
+
                 </div>
-                <div class="contact-img">
-                    <img src="{{asset('img/cause-3.png')}}" alt="Image" />
+            <form name="sentMessage" id="contactForm" novalidate="novalidate" action="{{route('contacts.store')}}" method="POST" {{-- id="formId" --}}>
+                @csrf
+                <div class="control-group">
+                    <input type="text" class="form-control" id="name" name="name" placeholder="Votre Nom" required="required" @error('name') is-invalid @enderror value="{{old('name')}}" data-validation-required-message="Please enter your name" novalidate="novalidate" />
+                    <p class="help-block text-danger">
+
+                        @error('name')
+                        {{$message}}
+                        @enderror
+                    </p>
                 </div>
-               <div class="contact-form">
-             
-                 <div id="success">
-       
-                  @if(session('success'))
-                
-                   <div class="alert alert-success">   {{session('success')}}</div>
-                  @endif
-                  
+                <div class="control-group">
+                    <input class="form-control" id="email" name="email" placeholder="Votre Telephone ou Email" required="required" value="{{old('email')}}" data-validation-required-message="Please enter your email" />
+                    <p class="help-block text-danger">
+                        @error('email')
+                        {{$message}}
+                        @enderror
+                    </p>
+                </div>
+                <div class="control-group">
+                    <input type="text" class="form-control" id="subject" name="subject" placeholder="Object" required="required" value="{{old('subject')}}" data-validation-required-message="Please enter a subject" />
+                    <p class="help-block text-danger">
+                        @error('subject')
+                        {{$message}}
+                        @enderror
+                    </p>
+                </div>
+                <div class="control-group">
+                    <textarea class="form-control" id="user_message" name="user_message" placeholder="Message" required="required" value="{{old('user_message')}}" data-validation-required-message="Please enter your user_message"></textarea>
+                    <p class="help-block text-danger">
+                        @error('user_message')
+                        {{$message}}
+                        @enderror
+                    </p>
+                </div>
+                <div>
+                    <button class="btn btn-custom" type="submit" onclick="submitClicked()" id="sendMessageButton">
+                        Envoyer Message
+                    </button>
+                </div>
+            </form>
                  </div>
-                <form
-                        name="sentMessage"
-                        id="contactForm"
-                        novalidate="novalidate"
-                        action="{{route('contacts.store')}}"
-                        method="POST"
-                        {{-- id="formId" --}}
-                    >
-                    @csrf
-                        <div class="control-group">
-                            <input
-                                type="text"
-                                class="form-control"
-                                id="name"
-                                name="name"
-                                placeholder="Votre Nom"
-                                required="required"
-                                @error('name') is-invalid @enderror
-                                value="{{old('name')}}"
-                                data-validation-required-message="Please enter your name"
-                                novalidate="novalidate"
-                            />
-                            <p class="help-block text-danger"> 
-                                
-                               @error('name')
-                                   {{$message}}
-                               @enderror
-                            </p>
-                        </div>
-                        <div class="control-group">
-                            <input
-                                
-                                class="form-control"
-                                id="email"
-                                name="email"
-                                placeholder="Votre Telephone ou Email"
-                                required="required"
-                                value="{{old('email')}}"
-                                data-validation-required-message="Please enter your email"
-                            />
-                            <p class="help-block text-danger">
-                                @error('email')
-                                   {{$message}}
-                               @enderror
-                            </p>
-                        </div>
-                        <div class="control-group">
-                            <input
-                                type="text"
-                                class="form-control"
-                                id="subject"
-                                name="subject"
-                                placeholder="Object"
-                                required="required"
-                                value="{{old('subject')}}"
-                                data-validation-required-message="Please enter a subject"
-                            />
-                            <p class="help-block text-danger">
-                                @error('subject')
-                                   {{$message}}
-                               @enderror
-                            </p>
-                        </div>
-                        <div class="control-group">
-                            <textarea
-                                class="form-control"
-                                id="user_message"
-                                name="user_message"
-                                placeholder="Message"
-                                required="required"
-                                value="{{old('user_message')}}"
-                                data-validation-required-message="Please enter your user_message"
-                            ></textarea>
-                            <p class="help-block text-danger">
-                                @error('user_message')
-                                   {{$message}}
-                                @enderror
-                            </p>
-                        </div>
-                        <div>
-                            <button
-                                class="btn btn-custom"
-                                type="submit"
-                                onclick="submitClicked()"
-                                id="sendMessageButton"
-                            >
-                                Envoyer Message
-                            </button>
-                        </div>
-                    </form>
-                </div>
             </div>
-        </div>
-        <!-- Contacts End -->
+            <div class="col-6 socials-contact contact-form" >
+                <h4 class="text-center">Nous contacter</h4>
+               
+            </div>
+    </div>
+       
+    </div>
+</div>
+<!-- Contacts End -->
 
 @include('partials.contacts.submit-modal')
 
