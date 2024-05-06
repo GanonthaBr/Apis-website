8645a1ea07e84212f375db080752be7041d6db36
Bruno <bganontha@gmail.com>
2024-05-06 17:12:30 +0100

    Update admin_home.blade.php to include new logo and change site name

--- resources/views/admin_home.blade.php
+++ resources/views/admin_home.blade.php
@@ -8,8 +8,8 @@
 
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
-        <img src="assets/img/logo.png" alt="">
-        <span class="d-none d-lg-block">NiceAdmin</span>
+        <img src="assets/img/apis.png" alt="">
+        <span class="d-none d-lg-block">ONG APIS</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
