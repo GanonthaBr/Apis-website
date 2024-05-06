4f73707260b72a443c0f9058848d953af8610d11
Bruno <bganontha@gmail.com>
2024-05-06 16:45:53 +0100

    Update admin_home.blade.php to include footer partial

---
+++ resources/views/partials/admin/footer.blade.php
@@ -0,0 +1,11 @@
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+    <div class="copyright">
+    {{Date('Y')}} &copy <strong><span> <a href="https://apis-sahel.org">Apis Sahel  </a></span></strong>.Tout Droit réservé.            
+    </div>
+    <div class="credits">
+        Conçu  par
+        <a href="https://apis-sahel.org">Apis Sahel</a>            
+    </div>
+  </footer><!-- End Footer -->
