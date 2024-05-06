c6c542411989e02e272938f68e3a1d5d60daa8f1
Bruno <bganontha@gmail.com>
2024-05-06 09:25:53 +0100

    Update topbar.blade.php to include address in the top bar

--- resources/views/includes/topbar.blade.php
+++ resources/views/includes/topbar.blade.php
@@ -5,7 +5,9 @@
                         <div class="top-bar-left">
                             <div class="text">
                                 {{-- <i class="fa fa-phone-alt"></i> --}}
-                                <p>ACTIONS ET PROGRAMMES <br> D'IMPACT AU SAHEL</p>
+                                {{-- <p>ACTIONS ET PROGRAMMES <br> D'IMPACT AU SAHEL</p> --}}
+                                   <i class="fa fa-map-marker-alt"></i>
+                                <p>Avenue Korey bongou, Niamey-Niger</p>
                             </div>
                             <div class="text">
                                 <i class="fa fa-phone-alt"></i>
