d274f4230b30505f07617e32e2186ae8bcf4617c
Bruno <bganontha@gmail.com>
2024-05-14 11:51:27 +0100

    refactor: Update user profile header image path and fallback

--- resources/views/partials/admin/header.blade.php
+++ resources/views/partials/admin/header.blade.php
@@ -161,7 +161,11 @@ $countm = 0;
       <li class="nav-item dropdown pe-3">
 
         <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+          @if ($user->image)
           <img src="{{asset('images/' . $user->image)}}" alt="Profile" class="rounded-circle">
+          @else
+          <img src="{{asset('assets/img/apis.png')}}" alt="Profile" class="rounded-circle">
+          @endif
           <span class="d-none d-md-block dropdown-toggle ps-2">Admin-{{$user->name}}</span>
         </a>
         <!-- End Profile Iamge Icon -->
