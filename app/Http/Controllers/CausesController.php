575e0f2ebc808c9ec0e3d92ace901b63c7f5c97e
Bruno <bganontha@gmail.com>
2024-05-16 10:40:12 +0100

    refactor: Add lazy loading for images in CausesController and Causes model

--- app/Http/Controllers/CausesController.php
+++ app/Http/Controllers/CausesController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Causes;
 use Illuminate\Http\Request;
 
 class CausesController extends Controller
 {
-    //
+    public function index()
+    {
+        $causes = Causes::all();
+        return view('partials.causes', ['causes' => $causes]);
+    }
 }
--- app/Models/Causes.php
+++ app/Models/Causes.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Causes extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'image',
+    ];
 }
