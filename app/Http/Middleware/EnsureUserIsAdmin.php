ae07aed93ebf47b3af1873125b86ba1526e0a5a3
Bruno <bganontha@gmail.com>
2024-06-03 16:42:02 +0100

    ensure user is Admin

--- app/Http/Middleware/EnsureUserIsAdmin.php
+++ app/Http/Middleware/EnsureUserIsAdmin.php
@@ -13,6 +13,7 @@ class EnsureUserIsAdmin
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
+
     public function handle(Request $request, Closure $next): Response
     {
         if (auth()->check() && auth()->user()) {
