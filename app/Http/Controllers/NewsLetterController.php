6e73a78cb7eb678e55235b1c8909e6e1be7c0100
Bruno <bganontha@gmail.com>
2024-07-18 10:13:38 +0100

    Fix validation error handling in NewsLetterController

--- app/Http/Controllers/NewsLetterController.php
+++ app/Http/Controllers/NewsLetterController.php
@@ -10,14 +10,18 @@ class NewsLetterController extends Controller
     // subscribe
     public function subscribe(Request $request)
     {
-        $request->validate([
-            'email' => 'required|email'
-        ]);
+        try {
+            $request->validate([
+                'email' => 'required|email'
+            ]);
 
-        // save email to database
-        $newsletter = new Newsletter();
-        $newsletter->email = $request->email;
-        $newsletter->save();
-        return back()->with('success', 'You have been successfully subscribed to our newsletter');
+            // save email to database
+            $newsletter = new Newsletter();
+            $newsletter->email = $request->email;
+            $newsletter->save();
+            return back()->with('success', 'You have been successfully subscribed to our newsletter');
+        } catch (\Throwable $e) {
+            return back()->with('error', 'Something went wrong, please try again');
+        }
     }
 }
