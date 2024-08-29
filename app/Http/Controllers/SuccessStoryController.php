b29dcdf8b97aac86b41fda59c6c24b88955b1ed9
Bruno <bganontha@gmail.com>
2024-08-29 10:29:21 +0100

    Implementing the Controller...

--- app/Http/Controllers/SuccessStoryController.php
+++ app/Http/Controllers/SuccessStoryController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SuccessStory;
 use Illuminate\Http\Request;
 
 class SuccessStoryController extends Controller
 {
-    //
+    public function index()
+    {
+        $success_story_list = SuccessStory::all();
+
+        return view('partails.success_story.index', ['success_story_list' => $success_story_list]);
+    }
 }
