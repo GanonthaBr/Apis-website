db0422af436ab0313ca1dc1655bf5a60de871631
Bruno <bganontha@gmail.com>
2024-09-05 11:32:27 +0100

    Display Latest Success Story video on the home page

--- app/Providers/BlogServiceProvider.php
+++ app/Providers/BlogServiceProvider.php
@@ -20,9 +20,10 @@ class BlogServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        view()->composer(['includes.sidebar', 'partials.admin.header', 'partials.admin.messages_list', 'admin_home', 'partials.admin.right_side_column'], function ($view) {
+        view()->composer(['includes.sidebar', 'partials.admin.header', 'partials.admin.messages_list', 'admin_home', 'partials.admin.right_side_column', 'partials.success_story.latest_success_story_videos'], function ($view) {
             $view->with('blogs', \App\Models\Blog::orderBy('created_at', 'desc')->take(3)->get())
                 ->with('events', \App\Models\Events::all()->take(2))
+                ->with('success_stories', \App\Models\SuccessStory::orderBy('created_at', 'desc')->take(1)->get())
                 ->with('messages', \App\Models\Contact::latest()->get()->map(
                     function ($message) {
                         $message->created_at_human = $message->created_at->locale('fr')->diffForHumans();
--- resources/views/partials/success_story/latest_success_story_videos.blade.php
+++ resources/views/partials/success_story/latest_success_story_videos.blade.php
@@ -9,12 +9,14 @@
 
     </div>
     <div class="row p-4">
+        @foreach($success_stories as $success_story)
         <div class="col-lg-1"></div>
         <div class="col-lg-10 m-4" style="width: 300px">
             <div class="embed-responsive embed-responsive-16by9">
-                <iframe width="200" height="100" src="https://www.youtube.com/embed/YO4h8zVUNHU?si=xsZavUe0DzBL3ORt" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="200" height="100" src="{{$success_story->video}}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </div>
         <div class="col-lg-1"></div>
+        @endforeach
     </div>
 </div>
